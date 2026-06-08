05b8cc0c91868b7a718cde56f66b843d5ccb4253
TaranenkoKen <kl991623@gmail.com>
2026-06-08 19:58:10 +0300

    some small fix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
 const mobileMenuLinkRef = document.querySelectorAll('.mobile-nav-link');
+const mobileMenuButton = document.querySelector('.mobile-menu__button');
 
 menuBtnRef.addEventListener('click', () => {
 	window.scrollTo(0, 0);
@@ -23,6 +24,15 @@ mobileMenuLinkRef.forEach((ref) => {
 		menuBtnRef.classList.remove('is-open');
 		mobileMenuRef.classList.remove('is-open');
 	});
+});
+
+mobileMenuButton.addEventListener('click', (event) => {
+	menuBtnRef.setAttribute('aria-expanded', false);
+
+	document.body.classList.remove('menu-open');
+
+	menuBtnRef.classList.remove('is-open');
+	mobileMenuRef.classList.remove('is-open');
 });
 
 window.addEventListener('resize', () => {
